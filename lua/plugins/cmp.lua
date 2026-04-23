d1c4b649f7864649294da635922d3ae1a1a1aa59
Viktor Herzog <vherzog@gmx.de>
2026-04-23 09:43:37 +0200

    Anpassung cmp-Mapping und das Verhalten bei Funktionen mit ... Parametern

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -26,16 +26,16 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({
-			select = false
+		['<CR>'] = cmp.mapping({
+			i = function(fallback)
+				if cmp.visible() and cmp.get_active_entry() then
+					cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+				else
+					fallback()
+				end
+			end,
+			s = cmp.mapping.confirm({ select = true }),
 		}),
-        ["<C-p>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end, {"i", "s"}),
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
